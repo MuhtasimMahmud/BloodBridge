acebd84c20cd137d74582a0ae2229df6462b26fd
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2024-08-20 03:52:45 +0600

    added post entity

---
+++ src/main/java/com/project/BloodBridge/model/Post.java
@@ -0,0 +1,36 @@
+package com.project.BloodBridge.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int postId;
+    private String bloodGroup;
+    private int bloodAmount;
+    private LocalDate donationDate;
+    private String contact;
+    private String upazillaOrThana;
+    private String district;
+    private String division;
+
+    @ManyToOne
+    @JsonBackReference
+    User user = new User();
+
+}
---
+++ src/main/java/com/project/BloodBridge/model/User.java
@@ -0,0 +1,35 @@
+package com.project.BloodBridge.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "user")
+public class User {
+
+    @Id
+    private String phoneNumber;
+    private String email;
+    private String bloodGroup;
+    private LocalDate lastDonationDate;
+    private String upazillaOrThana;
+    private String disctrict;
+    private String division;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user")
+    @JsonManagedReference
+    List<Post> posts = new ArrayList<>();
+
+}
